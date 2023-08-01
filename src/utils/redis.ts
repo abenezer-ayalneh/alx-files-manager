bde1e1564deefc61db34ca8ac3393a5a32226a4b
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2023-08-01 07:04:30 +0300

    chore: first commit

---
+++ src/utils/redis.ts
@@ -0,0 +1,35 @@
+import { RedisCommandArgument } from '@redis/client/dist/lib/commands'
+import { RedisClientType, createClient } from 'redis'
+require('dotenv').config()
+
+class RedisClient {
+  redisClient: RedisClientType
+
+  constructor() {
+    this.redisClient = createClient({
+      url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    })
+    this.redisClient.on('error', (err) => console.log('Redis Client Error', err))
+    this.redisClient.connect()
+  }
+
+  async isAlive() {
+    return this.redisClient.isReady
+  }
+
+  async get(key: string) {
+    const value = await this.redisClient.get(key)
+    return value
+  }
+
+  async set(key: RedisCommandArgument, value: number | RedisCommandArgument, duration: number = 5) {
+    await this.redisClient.set(key, value, { EX: duration })
+  }
+
+  async del(key: RedisCommandArgument) {
+    await this.redisClient.del(key)
+  }
+}
+
+const redisClient = new RedisClient()
+export default redisClient
